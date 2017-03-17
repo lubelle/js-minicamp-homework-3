fa52f09abe32cd09f8b5e6a42af039595fa5f798
Belle <lxliu@student.oaklandcc.edu>
2017-03-16 20:04:15 -0400

    Assignment#3

--- exercises.js
+++ exercises.js
@@ -7,61 +7,108 @@ function makeCat(name, age) {
   //add an age property to the object with the value set to the age argument
   //add a method called meow that returns the string 'Meow!'
   //return the object
+  var cat = {};
+  cat.name = name;
+  cat.age = age;
+  cat.meow = function(){
+    return 'Meow!';
+  };
+  return cat;
 }
 
 function addProperty(object, property) {
   //add the property to the object with a value of null
   //return the object
   //note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  var propertyName = property;
+  object[propertyName] = null;
+  return object;
 }
 
 function invokeMethod(object, method) {
   //method is a string that contains the name of a method on the object
   //invoke this method
   //nothing needs to be returned
+  var methodName = method;
+  object[methodName]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   //mysteryNumberObject has a property called mysteryNumber
   //multiply the mysteryNumber property by 5 and return the product
+  var obj = {};
+  obj = mysteryNumberObject;
+  obj.mysteryNumber = obj.mysteryNumber * 5;
+  return obj.mysteryNumber;
 }
 
 function deleteProperty(object, property) {
   //remove the property from the object
   //return the object
+  var propertyName = property;
+  delete object[propertyName];
+  return object;
 }
 
 function newUser(name, email, password) {
   //create a new object with properties matching the arguments passed in.
   //return the new object
+  var obj = {};
+  obj.name = name;
+  obj.email = email;
+  obj.password = password;
+  return obj;
 }
 
 function hasEmail(user) {
   //return true if the user has a value for the property 'email'
   //otherwise return false
+  if(user.email){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function hasProperty(object, property) {
   //return true if the object has the value of the property argument
   //property is a string
   //otherwise return false
+  var propertyName = property;
+  if (propertyName in object){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function verifyPassword(user, password) {
   //check to see if the provided password matches the password property on the user object
   //return true if they match
   //otherwise return false
+  var pass = password;
+  if (user.password === pass){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function updatePassword(user, newPassword) {
   //replace the existing password on the user object with the value of newPassword
   //return the object
+  var pass = newPassword;
+  user.password = pass;
+  return user;
 }
 
 function addFriend(user, newFriend) {
   //user has a property called friends that is an array
   //add newFriend to the end of the friends array
   //return the user object
+  var newElement = newFriend;
+  user.friends.push(newElement);
+  return user;
 }
 
 function setUsersToPremium(users) {
@@ -69,6 +116,10 @@ function setUsersToPremium(users) {
   //each user object has the property 'isPremium'
   //set each user's isPremium property to true
   //return the users array
+  for (var i = 0; i < users.length; i++) {
+    users[i].isPremium = true;
+  }
+  return users;
 }
 
 function sumUserPostLikes(user) {
@@ -77,6 +128,11 @@ function sumUserPostLikes(user) {
   //each post object has an integer property called 'likes'
   //sum together the likes from all the post objects
   //return the sum
+  var count = 0;
+  user.posts.forEach(function(obj){
+    count = count + obj.likes;
+  });
+  return count;
 }
 
 function addCalculateDiscountPriceMethod(storeItem) {
@@ -87,6 +143,11 @@ function addCalculateDiscountPriceMethod(storeItem) {
   //price -> 20
   //discountPercentage -> .2
   //discountPrice = 20 - (20 * .2)
+  storeItem.calculateDiscountPrice = function(){
+    var discounPrice = storeItem.price - (storeItem.price * storeItem.discountPercentage);
+    return discounPrice;
+  };
+  return storeItem;
 }
 
 //Do not modify code below this line.
